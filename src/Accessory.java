914a2234a3a0f7adfa46ba01a72962c73c19dbf3
Mingfei Shao <tshao87@gmail.com>
2017-07-10 23:18:39 -0500

    sync with github

---
+++ src/Accessory.java
@@ -0,0 +1,12 @@
+public class Accessory extends Product {
+
+	public Accessory(String name, double price, String image, String manufacturer, String condition, double discount) {
+		super();
+		this.setCategory("accessory");
+	}
+
+	public Accessory() {
+		super();
+		this.setCategory("accessory");
+	}
+}
